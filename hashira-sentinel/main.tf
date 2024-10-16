feb87a88e444b639bb1a58ea8ed24948038fbfe0
awani-mayaki <ibrahim.mayaki@1nce.com>
2024-10-16 09:42:21 -0400

    deploy worskpace and log analytics

--- hashira-sentinel/main.tf
+++ hashira-sentinel/main.tf
@@ -37,3 +37,59 @@ resource "azurerm_log_analytics_solution" "secu8090" {
   }
 }
 
+# Enable Sentinel Training Lab Solution
+module "mod_training_lab" {
+  depends_on = [ azurerm_sentinel_log_analytics_workspace_onboarding.secu8090 ]
+  source  = "azurenoops/overlays-arm-deployment/azurerm//modules/azure_arm_deployment/resource_group"
+  version = "~> 1.0"
+  count   = var.enable_sentinel && var.enable_solution_training_lab ? 1 : 0
+
+  name                = "hashira-sentinel-training-lab-content-solution"
+  resource_group_name = azurerm_resource_group.secu8090.name
+  deployment_mode     = var.deployment_mode
+  deploy_environment  = var.deploy_environment
+  workload_name       = "solutions"
+
+  arm_script = file("${path.module}/sentinel/training_lab.json")
+
+  parameters_override = {
+    "workspaceName" = azurerm_log_analytics_workspace.secu8090.name
+    "location"      = azurerm_resource_group.secu8090.location
+  }
+}
+
+variable "enable_sentinel" {
+  type        = bool
+  default     = true
+  description = "Set to false to prevent the module from creating any sentinel resources."
+}
+
+variable "enable_solution_training_lab" {
+  description = "Enable Training Lab Solution in Sentinel. Default is false."
+  type        = bool
+  default     = true
+}
+
+variable "enable_sentinel_onboarding" {
+  type        = bool
+  default     = true
+  description = "Set to false to prevent the module from onboarding the Log Analytics Workspace to Azure Sentinel."
+}
+
+variable "deployment_mode" {
+  # https://docs.microsoft.com/en-us/azure/azure-resource-manager/templates/deployment-modes
+  description = "The Deployment Mode for this Resource Group Template Deployment. Possible values are Complete (where resources in the Resource Group not specified in the ARM Template will be destroyed) and Incremental (where resources are additive only). This is only used in Hub Content Solutions."
+  type        = string
+  default     = "Incremental"
+
+  validation {
+    condition     = contains(["Incremental", "Complete"], var.deployment_mode)
+    error_message = "This value must be either Incremental or Complete."
+  }
+}
+
+variable "deploy_environment" {
+  description = "Name of the workload's environnement (dev, test, prod, etc). This will be used to name the resources deployed by this module. default is 'dev'"
+  type        = string
+  default = "production"
+}
